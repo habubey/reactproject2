f609dbe935ad7679621dae1d8f9f48503c22455a
habubey <habubey@outlook.com>
2022-04-14 23:12:24 +0300

    react

---
+++ src/components/Header.js
@@ -0,0 +1,20 @@
+
+import Button from "./Button";
+
+const Header = ({ title, showAddTask, toggleShow }) => {
+
+  return (
+    <div className="header">
+      <h1>{title}</h1>
+   
+      <Button
+        color={showAddTask ? "red" : "purple"}
+        text={showAddTask ? "Close Add Task Bar" : "Show Add Task Bar"}
+        toggleShow={toggleShow}
+      />
+    </div>
+  );
+};
+
+
+export default Header;
